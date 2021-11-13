dbee357ba0d089453f9167562682e36a34f55fd1
laptop <juha.jylhakoski@aalto.fi>
2021-11-13 10:09:26 +0800

    1.03

--- 1/1.03/index.js
+++ 1/1.03/index.js
@@ -4,10 +4,30 @@ const app = new Koa()
 
 const PORT = process.env.PORT || 3000
 
-const createRandomString = () => Math.random().toString(36).substr(2, 6)
+const getHashNow = () => {
+  
+  const randomhash = Math.random().toString(36).substr(2, 6)
 
-const startingString = createRandomString()
+  return randomhash
+
+  //setTimeout(getHashNow, 5000)
+}
+
+const randomhash = getHashNow()
+
+const loop = () => {
+
+  const timestamp = new Date().toISOString()
+
+  console.log(timestamp + ' ' + randomhash)
+
+  setTimeout(loop, 5000)
+
+}
 
 app.listen(PORT)
 
 console.log(`Server started in port ${PORT}`)
+
+loop()
+
