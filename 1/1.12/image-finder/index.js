c47c99363451787f08c2a62e7210c4e4479a9c71
laptop <juha.jylhakoski@aalto.fi>
2021-11-15 08:09:03 +0800

    1.12

---
+++ 1/1.12/image-finder/index.js
@@ -0,0 +1,56 @@
+const Koa = require('koa')
+
+const path = require('path')
+
+const fs = require('fs')
+
+const axios = require('axios')
+
+const app = new Koa()
+
+const PORT = process.env.PORT || 3001
+
+const directory = path.join('/', 'usr', 'src', 'app', 'files')
+
+const filePath = path.join(directory, 'image.jpg')
+
+const fileAlreadyExists = async () => new Promise(res => {
+
+  fs.stat(filePath, (err, stats) => {
+
+    if (err || !stats) return res(false)
+
+    return res(true)
+  })
+})
+
+const findAFile = async () => {
+
+  if (await fileAlreadyExists()) return
+
+  await new Promise(res => fs.mkdir(directory, (err) => res()))
+
+  const response = await axios.get('https://picsum.photos/1200', { responseType: 'stream' })
+
+  response.data.pipe(fs.createWriteStream(filePath))
+}
+
+const removeFile = async () => new Promise(res => fs.unlink(filePath, (err) => res()))
+
+app.use(async ctx => {
+
+  if (ctx.path.includes('favicon.ico')) return
+  
+  await removeFile()
+
+  findAFile()
+
+  ctx.status = 200
+});
+
+findAFile()
+
+app.listen(PORT)
+
+console.log('Image-Finder Started: ' + PORT)
+
---
+++ 1/1.12/image-response/index.js
@@ -0,0 +1,41 @@
+const Koa = require('koa')
+
+const path = require('path')
+
+const fs = require('fs')
+
+const app = new Koa()
+
+const PORT = process.env.PORT || 3000
+
+const directory = path.join('/', 'usr', 'src', 'app', 'files')
+
+const filePath = path.join(directory, 'image.jpg')
+
+const getFile = async () => new Promise(res => {
+
+  fs.readFile(filePath, (err, buffer) => {
+
+    if (err) return console.log('FAILED TO READ FILE', '----------------', err)
+
+    res(buffer)
+  })
+})
+
+app.use(async ctx => {
+
+  if (ctx.path.includes('favicon.ico')) return
+
+  ctx.body = await getFile()
+
+  ctx.set('Content-disposition', 'attachment; filename=image.jpg');
+
+  ctx.set('Content-type', 'image/jpeg');
+
+  ctx.status = 200
+});
+
+app.listen(PORT)
+
+console.log('Image-Response Started: ' + PORT)
+
